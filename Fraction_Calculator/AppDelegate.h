ea05101c41c5bd63115dccf98c5d1de85270d427
LinChiSin <hey@linchisin.com>
2018-06-21 11:37:17 +0800

    Initial Commit

---
+++ Fraction_Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fraction_Calculator
+//
+//  Created by zhixinglin on 2018/6/21.
+//  Copyright © 2018 zhixinglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fraction_Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fraction_Calculator
+//
+//  Created by zhixinglin on 2018/6/21.
+//  Copyright © 2018 zhixinglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
